e7ae882ccdbdf6ec193d54068b321df248f3f21f
Andrés Martínez <aemb87@gmail.com>
2017-03-17 15:39:53 -0300

    Fix npm packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import MainView from './views/MainView';
 import SubView from './views/SubView';
@@ -11,7 +11,7 @@ const App = () => (
       		<Route exact path="/" component={MainView}/>
       		<Route path="/about" component={SubView}/>
       	</div>
-  </Router>
+    </Router>
 )
 
 document.addEventListener('DOMContentLoaded', function(){
